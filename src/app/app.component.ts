2dca203063905da62f3b63519628065eca8f9350
sravan4205 <sravan.amishetti@gmail.com>
2020-02-15 19:27:36 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Chart} from 'chart.js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,128 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'charts';
+  title: 'Charts';
+  LineChart = [];
+  BarChart = [];
+  PieChart = [];
+
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnInit() {
+     // Line chart:
+this.LineChart = new Chart('lineChart', {
+  type: 'line',
+data: {
+ labels: ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+ datasets: [{
+     label: 'Number of Items Sold in Months',
+     data: [9, 7 , 3, 5, 2, 10, 15, 16, 19, 3, 1, 9],
+     fill: false,
+     lineTension: 0.2,
+     borderColor: 'red',
+     borderWidth: 1
+ }]
+},
+options: {
+  responsive: true,
+ title: {
+     text: 'Line Chart',
+     display: true
+ },
+ scales: {
+     yAxes: [{
+         ticks: {
+             beginAtZero: true
+         }
+     }]
+ }
+}
+});
+
+// Bar chart:
+this.BarChart = new Chart('barChart', {
+  type: 'bar',
+data: {
+ labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+ datasets: [{
+     label: '# of Votes',
+     data: [9, 7 , 3, 5, 2, 10],
+     backgroundColor: [
+         'rgba(255, 99, 132, 0.2)',
+         'rgba(54, 162, 235, 0.2)',
+         'rgba(255, 206, 86, 0.2)',
+         'rgba(75, 192, 192, 0.2)',
+         'rgba(153, 102, 255, 0.2)',
+         'rgba(255, 159, 64, 0.2)'
+     ],
+     borderColor: [
+         'rgba(255,99,132,1)',
+         'rgba(54, 162, 235, 1)',
+         'rgba(255, 206, 86, 1)',
+         'rgba(75, 192, 192, 1)',
+         'rgba(153, 102, 255, 1)',
+         'rgba(255, 159, 64, 1)'
+     ],
+     borderWidth: 1
+ }]
+},
+options: {
+  responsive: true,
+ title: {
+     text: 'Bar Chart',
+     display: true
+ },
+ scales: {
+     yAxes: [{
+         ticks: {
+             beginAtZero: true
+         }
+     }]
+ }
+}
+});
+
+// pie chart:
+this.PieChart = new Chart('pieChart', {
+  type: 'pie',
+data: {
+ labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+ datasets: [{
+     label: '# of Votes',
+     data: [9, 7 , 3, 5, 2, 10],
+     backgroundColor: [
+         'rgba(255, 99, 132, 0.2)',
+         'rgba(54, 162, 235, 0.2)',
+         'rgba(255, 206, 86, 0.2)',
+         'rgba(75, 192, 192, 0.2)',
+         'rgba(153, 102, 255, 0.2)',
+         'rgba(255, 159, 64, 0.2)'
+     ],
+     borderColor: [
+         'rgba(255,99,132,1)',
+         'rgba(54, 162, 235, 1)',
+         'rgba(255, 206, 86, 1)',
+         'rgba(75, 192, 192, 1)',
+         'rgba(153, 102, 255, 1)',
+         'rgba(255, 159, 64, 1)'
+     ],
+     borderWidth: 1
+ }]
+},
+options: {
+ title: {
+     text: 'Pie Chart',
+     display: true
+ },
+ scales: {
+     yAxes: [{
+         ticks: {
+             beginAtZero: true
+         }
+     }]
+ }
+}
+});
+
+
+}
 }
